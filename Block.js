1f7d8b6906f9f679792ee6f42bea799e6f8f4cfb
tia-sharma <77229942+tia-sharma@users.noreply.github.com>
2021-05-23 11:34:49 +0100

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,38 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :1.0,
+          
+      }
+
+      this.image=loadImage("block.png")
+          this.visibility = 255;
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      
+      
+        if(this.body.speed<3){
+          var angle = this.body.angle;
+          var pos = this.body.position;
+          push();
+          translate(pos.x,pos.y);
+          rotate(angle);
+          rectMode(CENTER);
+          rect(pos.x,pos.y,this.width, this.height);
+          pop();
+        } else{          
+          World.remove(world,this.body);
+          push()
+          this.body.visibility=this.body.visibility-5;
+          tint(255,this.visibility)
+          image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+          pop()
+        }
+      }
+    
+}
